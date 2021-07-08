42674f9ff6b8df49552753aa8a0926a3390ec2e3
Steve MacPherson <stevemacpherson@rogers.com>
2021-07-07 21:44:56 -0400

    deliverable2

--- Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Employee_Database_Challenge.sql
+++ Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Employee_Database_Challenge.sql
@@ -28,3 +28,30 @@ FROM unique_titles
 GROUP BY title
 ORDER BY COUNT DESC
 ;
+
+-- Deliverable 2 --
+SELECT	DISTINCT ON (e.emp_no)
+		e.emp_no,
+		e.first_name,
+		e.last_name,
+		e.birth_date,
+		de.from_date,
+		de.to_date,
+		t.title
+INTO mentorship_eligibilty
+FROM employees as e
+	LEFT JOIN dept_emp as de
+		ON e.emp_no=de.emp_no
+	LEFT JOIN titles as t
+		ON e.emp_no=t.emp_no
+WHERE (de.to_date='9999-01-01') 
+	 AND (e.birth_date>='1965-01-01' AND e.birth_date<='1965-12-31')
+ORDER BY e.emp_no ASC
+;
+
+
+
+
+
+
+
